6feaf9476481e012fd8363b8f5430cbc1af8d974
Prashanth <prshnth.619@gmail.com>
2024-05-12 21:17:10 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  //   Write your code here.
+  <div class='container'>
+    <h1>Super Over League</h1>
+    <img
+      src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+      class='image'
+    />
+    <img
+      src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+      class='image'
+    />
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
